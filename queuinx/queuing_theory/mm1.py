28253e067e05bdb9bea33cfe33105451a3167f29
Krzysztof Rusek <krusek@agh.edu.pl>
2023-02-23 10:01:24 +0100

    Add mm1 system

---
+++ queuinx/queuing_theory/mm1.py
@@ -0,0 +1,30 @@
+#   Copyright 2023 Krzysztof Rusek
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import chex
+import distrax
+import tensorflow_probability.substrates.jax as tfp
+tfd = tfp.distributions
+
+def delay_distribution(a: chex.Array, mu: chex.Array) -> distrax.DistributionLike:
+    """Return delay distribution in M/M/1 for customers who arrive and find the queue as a stationary process [1]_.
+    This includes both waiting and service time.
+
+    :param a: Arrival rate
+    :param mu: Service rate
+    :return: Delay distribution (including service time)
+
+    .. [1] Harrison, P. G. (1993). "Response time distributions in queueing network models". Performance Evaluation of Computer and Communication Systems. Lecture Notes in Computer Science. Vol. 729. pp. 147–164.
+    """
+    return tfd.Exponential(rate=mu-a)
---
+++ queuinx/queuing_theory/mm1_test.py
@@ -0,0 +1,36 @@
+#   Copyright 2023 Krzysztof Rusek
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import unittest
+import jax
+import jax.numpy as jnp
+import mm1 as qt
+class TestCase(unittest.TestCase):
+    def test_analytical(self):
+        _lambda = 2.
+        mu = 3.
+        dist = qt.delay_distribution(_lambda,mu)
+        self.assertAlmostEqual(dist.mean(),1./(mu-_lambda))
+
+    def test_vmap(self):
+        _lambda = 2. + jnp.ones(3)
+        mu = 3. + jnp.ones(3)
+        f = jax.vmap(lambda x,y: qt.delay_distribution(x,y).mean())
+        self.assertTrue(jnp.allclose(f(_lambda, mu),1./(mu-_lambda)))
+
+        f = jax.jit(jax.vmap(lambda x, y: qt.delay_distribution(x, y).variance()))
+        f(_lambda, mu)
+
+if __name__ == '__main__':
+    unittest.main()
