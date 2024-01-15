76d54e0f8bae44b260bf42723f4e3d8f40401966
Krzysztof Rusek <krusek@agh.edu.pl>
2024-01-15 16:06:47 +0100

    Polish doc

--- docs/conf.py
+++ docs/conf.py
@@ -2,14 +2,16 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+from datetime import date
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import queuinx
 project = 'Queuinx'
-copyright = '2023, Krzysztof Rusek'
+copyright = f'{date.today().year}, Krzysztof Rusek'
 author = 'Krzysztof Rusek'
-release = '0.0.1'
+version = queuinx.__version__
 
 import os
 import sys
@@ -32,6 +34,23 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    "repository_url": "https://github.com/krzysztofrusek/queuinx",
+    "use_repository_button": True,
+    "use_download_button": False,
+}
+
 html_static_path = ['_static']
+html_logo = "_static/images/logo.svg"
 autodoc_typehints = 'none'
 source_suffix = ['.rst', '.md', '.ipynb']
+
+# -- Options for autodoc ----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#configuration
+
+# Automatically extract typehints when specified and place them in
+# descriptions of the relevant function/method.
+autodoc_typehints = "description"
+
+# Don't show class signature with the class' name.
+autodoc_class_signature = "separated"
